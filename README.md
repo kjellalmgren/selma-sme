2ff9b5eb07687192cff12bb0624959181d2994a8
Kjell Almgren <kjell.almgren@tetracon.se>
2019-02-27 21:46:00 +0100

    Selmasme - minor changes

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Selma-SME
 
+## Docker login
+    tetracon/Pdx10p2017!
+
 ## Static REST-API
+
+##build container
+
+    $ GOARCH=amd64 CGO_ENABLED=0 go build -ldflags '-w -s' -a -installsuffix cgo -o selmasme
+    # build with tag selma.sme, -t equals tag
+    $ docker build --file Dockerfile.builder -t tetracon/selmasme:0.4.8 .
+    # push to hub.docker.com, assumes docker login
+    $ docker push tetracon/selma-sme:0.4.8
+    # run container version 0.4.8
+    $ docker run -it --name selmasme --publish=8000:8000 -t tetracon/selmasme:0.4.8
+    # run shell to look into cantainer
+    $ docker run -it -t tetracon/selmasme:0.4.8 sh
+
+    # docker service create --name=pingservices --publish=80:9000 tetracon/pingservices:2.19
--- main.go
+++ main.go
@@ -1,36 +1,69 @@
 package main
 
+//
+/*
+Services: selma-sme
+	Author: Kjell Osse Almgren, Tetracon AB
+	Date: 2019-02-27
+	Description: Service to feed Selma-SME UX, just for test purpose
+	Architecture:
+	win32: GOOS=windows GOARCH=386 go build -v
+	win64: GOOS=windows GOARCH=amd64 go build -v
+	arm64: GOOS=linux GOARCH=arm64 go build -v
+	arm: GOOS=linux GOARCH=arm go build -v
+	exprimental: GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -ldflags '-w -s' -a -installsuffix cgo -o selma-sme
+	expriemntal: CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -tags selma-sme -ldflags '-w'
+	exprimental: GOOS=linux GOARCH=arm64 go build -a --ldflags 'extldflags "-static"' -tags selma-sme -installsuffix selma-sme .
+*/
+
 import (
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 //
-//	Process
+//	Processes
+//
+// Banner text
+const (
+	// TETRACON banner
+	TETRACON = `
+_________    __
+|__    __|   | |
+   |  |  ___ | |_   ____  ___   ___ ___  _ __ 
+   |  | / _ \|  _| /  __|/ _ \ / __/ _ \| '_ \
+   |  | \ __/| |_  | |  | (_| | (_| (_) | | | | 
+   |__| \___| \__| |_|   \__,_|\___\___/|_| |_| 
+version: %s
+`
+)
+
+//
+var (
+	srv  bool
+	vrsn bool
+)
+
+// **************************************
 //
 type Process struct {
-	ProcessID  string `json:"processId"`
-	CustomerID string `json:"customerId,omitempty"`
+	ProcessID   string `json:"processId,omitempty"`
+	CustomerID  string `json:"customerId,omitempty"`
+	DateCreated string `json:"dateCreated,omitempty"`
 }
-
-//var processes []Process
-//var process Process
 
 // our main function
 func main() {
-	//process.ProcessID = "1"
-	//process.CustomerID = "Kjell Almgren"
-	//processes = append(processes, Process{ProcessID: "1", CustomerID: "19601005-0190"})
-	//processes = append(processes, Process{ProcessID: "2", CustomerID: "19710101-0120"})
-	//processes = append(processes, Process{ProcessID: "3", CustomerID: "19720101-0120"})
-	//processes = append(processes, Process{ProcessID: "4", CustomerID: "19730202-0240"})
+
+	fmt.Fprint(os.Stderr, fmt.Sprintf(TETRACON, PingVersion()))
+	fmt.Fprint(os.Stdout, fmt.Sprintf(getHostname()))
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/v1/Process", getProcess).Methods("GET", "OPTIONS")
 	router.HandleFunc("/v1/Processes/{customerId}", getProcesses).Methods("GET", "OPTIONS")
 	fmt.Printf("Listen on server localhost:8000\r\n")
 	err := http.ListenAndServe(":8000", router)
@@ -45,25 +78,6 @@ func main() {
 //
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
-}
-
-//
-// getProcess
-//
-func getProcess(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("getProcess executed...")
-
-	var process Process
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("Access-Control-Allow-Origin", "https://app.swaggerhub.com")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me")
-	if err := json.NewEncoder(w).Encode(process); err != nil {
-		panic(err)
-	}
-	w.WriteHeader(http.StatusOK)
 }
 
 //
@@ -86,9 +100,9 @@ func getProcesses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println()
 	switch vars["customerId"] {
 	case "19700101-1001":
-		processes = append(processes, Process{ProcessID: "11", CustomerID: "19700101-1001"})
+		processes = append(processes, Process{ProcessID: "11", CustomerID: "19700101-1001", DateCreated: "2019-02-27"})
 	case "19700101-2002":
-		processes = append(processes, Process{ProcessID: "22", CustomerID: "19700101-2002"})
+		processes = append(processes, Process{ProcessID: "22", CustomerID: "19700101-2002", DateCreated: "2019-02-20"})
 	default:
 		w.WriteHeader(http.StatusNotFound)
 	}
@@ -103,4 +117,23 @@ func getProcesses(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+//
+//	getHostName
+//
+func getHostname() string {
+
+	hostname, err1 := os.Hostname()
+	if err1 == nil {
+		//log.Printf("Hostname: %s", hostname)
+		fmt.Println("Error when try to resolve Hostname: ", hostname)
+	}
+	return hostname
+}
+
+//
+//	getVersion
+func PingVersion() string {
+	return "v0.4.8"
 }

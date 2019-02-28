a98304dc414b5fa61731f109599c15df60b299e2
Kjell Almgren <kjell.almgren@tetracon.se>
2019-02-28 16:22:41 +0100

    Minor changes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 /*
 Services: selma-sme
 	Author: Kjell Osse Almgren, Tetracon AB
-	Date: 2019-02-27
+	Date: 2019-02-28
 	Description: Service to feed Selma-SME UX, just for test purpose
 	Architecture:
 	win32: GOOS=windows GOARCH=386 go build -v

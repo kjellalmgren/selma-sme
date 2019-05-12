cc202403a77a4eea4ab32082cd55f845eebb71fd
Kjell Almgren <kjell.almgren@tetracon.se>
2019-05-12 09:42:06 +0200

    New environment version 1.12.5

--- version/version.go
+++ version/version.go
@@ -4,15 +4,15 @@ package version
 
 // ServerVersion - version number (exported)
 func ServerVersion() string {
-	return "v0.9.3"
+	return "v0.9.4"
 }
 
 // ModelVersion (Open API version)
 func ModelVersion() string {
-	return "v0.6.1"
+	return "v0.6.2"
 }
 
 // Model change date
 func ModelDate() string {
-	return "2019-05-08"
+	return "2019-05-12"
 }
